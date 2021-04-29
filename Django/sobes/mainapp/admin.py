b9c6ceb5520b62f0560a411ba421ebbcc4f1c87c
alekskositsyn <61062623+alekskositsyn@users.noreply.github.com>
2021-04-29 19:03:34 +0300

    создал поля по заданию

--- Django/sobes/mainapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import GoodItem
-
-admin.site.register(GoodItem)
-
--- Django/sobes/mainapp/urls.py
+++
@@ -1,8 +0,0 @@
-import mainapp.views as mainapp
-from django.urls import path
-
-app_name = 'mainapp'
-urlpatterns = [
-    path('', mainapp.goods_list, name='goods_list'),
-    path('good/create/', mainapp.good_create, name='good_create'),
-    ]
